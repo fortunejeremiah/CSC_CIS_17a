53d47c72849a8cffacc0f30022f47e16098cbcb5
fortunejeremiah <fortunejeremiah8@gmail.com>
2026-09-13 19:48:56 -0700

    Add files via upload

---
+++ Class/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * File: main.cpp
+ * Author: Jeremiah Fortune
+ * Created on September 13, 2026
+ * Purpose: Display Hello World
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
